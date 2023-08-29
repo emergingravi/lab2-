a2b6c2a1977839a437e9f596b1d9faebc133b539
callme_ravi <128369187+emergingravi@users.noreply.github.com>
2023-08-29 10:22:29 +0545

    Add files via upload

--- new.js
+++ new.js
@@ -4,9 +4,22 @@ function onsubmit(event){
     var name=document.getElementById("name").value;
     var email=document.getElementById("email").value;
     var gender=document.querySelector('input[name="gender"]:checked').value;
-    var license=document.
-    
-    document.getElementById("formData").innerHTML="Name : "+ name +" <br>Email :" + email +"<br> Gender : " + gender;
+    var acceptLicenseYes=document.getElementById("acceptLicenseYes");
+    var acceptLicenseN0=document.getElementById("acceptLicenseNo");
+    var acceptLicense;
+    if(acceptLicenseYes.checked==true && acceptLicenseNo.checked==true){
+        acceptLicense="cannot select both checkboxes";
+    }
+    else if(acceptLicenseYes.checked==true)
+    {
+        acceptLicense=document.getElementById("acceptLicenseYes").value;
+    }
+    else if(acceptLicenseN0.checked==true){
+        acceptLicense=document.getElementById("acceptLicenseNo").value;
+    }
+    var dropdown = document.getElementById("country");
+    var selectedOption = dropdown.options[dropdown.selectedIndex].text;
+    document.getElementById("formData").innerHTML="Name : "+ name +" <br>Email :" + email +"<br> Gender : " + gender +"<br> Accept Lisence " + acceptLicense + "<br> country : " + selectedOption;
 }
 window.onload=function(){
     // window.alert("hello .js");
